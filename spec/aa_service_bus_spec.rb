19cb262c83de46612f52305fd72f901dc8ea3e55
Hugo Corbucci <hugo.corbucci@gmail.com>
2018-09-09 09:52:17 -0400

    Fixing rubocop

--- spec/aa_service_bus_spec.rb
+++ spec/aa_service_bus_spec.rb
@@ -8,7 +8,7 @@ end
 
 RSpec.describe AaServiceBus, type: :request do
   describe 'GET /' do
-    before { get '/', {}, { 'HTTP_AUTHORIZATION' => '' } }
+    before { get '/', {}, 'HTTP_AUTHORIZATION' => '' }
     it { expect(last_response.body).to include 'Welcome to the Agile Alliance Service Bus' }
   end
 
@@ -27,7 +27,7 @@ RSpec.describe AaServiceBus, type: :request do
           subject(:body) { last_response.body }
           it 'responds true' do
             WebMock.stub_request(:post, "#{ENV['AA_API_HOST']}/check-membership").to_return(status: 200, body: '<?xml version=\"1.0\" encoding=\"UTF-8\"?><data><result>1</result></data>', headers: {})
-            get '/check_member/foo@bar.com', {}, { 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN'] }
+            get '/check_member/foo@bar.com', {}, 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN']
             expect(last_response.status).to eq 200
             expect(JSON.parse(body)['member']).to be_truthy
           end
@@ -37,7 +37,7 @@ RSpec.describe AaServiceBus, type: :request do
           subject(:body) { last_response.body }
           it 'responds false' do
             WebMock.stub_request(:post, "#{ENV['AA_API_HOST']}/check-membership").to_return(status: 200, body: '<?xml version=\"1.0\" encoding=\"UTF-8\"?><data><result>0</result></data>', headers: {})
-            get '/check_member/bla@xpto.com', {}, { 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN'] }
+            get '/check_member/bla@xpto.com', {}, 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN']
             expect(last_response.status).to eq 200
             expect(JSON.parse(body)['member']).to be_falsey
           end
@@ -47,7 +47,7 @@ RSpec.describe AaServiceBus, type: :request do
           subject(:body) { last_response.body }
           it 'responds false' do
             WebMock.stub_request(:post, "#{ENV['AA_API_HOST']}/check-membership").to_return(status: 200, body: '<?xml version=\"1.0\" encoding=\"UTF-8\"?><data></data>', headers: {})
-            get '/check_member/bla@xpto.com', {}, { 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN'] }
+            get '/check_member/bla@xpto.com', {}, 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN']
             expect(last_response.status).to eq 200
             expect(JSON.parse(body)['member']).to be_falsey
           end
@@ -56,7 +56,7 @@ RSpec.describe AaServiceBus, type: :request do
           subject(:body) { last_response.body }
           it 'responds false' do
             WebMock.stub_request(:post, "#{ENV['AA_API_HOST']}/check-membership").to_return(status: 200, body: '<?xml version=\"1.0\" encoding=\"UTF-8\"?>', headers: {})
-            get '/check_member/bla@xpto.com', {}, { 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN'] }
+            get '/check_member/bla@xpto.com', {}, 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN']
             expect(last_response.status).to eq 200
             expect(JSON.parse(body)['member']).to be_falsey
           end
@@ -70,7 +70,7 @@ RSpec.describe AaServiceBus, type: :request do
           it 'verifies on CSV file and returns true' do
             expect(File).to receive(:read) { file }
             WebMock.stub_request(:post, "#{ENV['AA_API_HOST']}/check-membership").to_raise(Net::OpenTimeout)
-            get '/check_member/bla@xpto.com', {}, { 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN'] }
+            get '/check_member/bla@xpto.com', {}, 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN']
             expect(last_response.status).to eq 200
             expect(JSON.parse(body)['member']).to be_truthy
           end
@@ -79,7 +79,7 @@ RSpec.describe AaServiceBus, type: :request do
           it 'verifies on CSV file and returns false' do
             expect(File).to receive(:read) { file }
             WebMock.stub_request(:post, "#{ENV['AA_API_HOST']}/check-membership").to_raise(Net::OpenTimeout)
-            get '/check_member/foo@bar.com', {}, { 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN'] }
+            get '/check_member/foo@bar.com', {}, 'HTTP_AUTHORIZATION' => ENV['AA_API_TOKEN']
             expect(last_response.status).to eq 200
             expect(JSON.parse(body)['member']).to be_falsey
           end
